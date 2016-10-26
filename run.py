0e679a0b0019ce6b2f5134da8b8038781c95a41a
lxz <253550465@qq.com>
2016-10-27 00:56:13 +0800

    2016-10-27 00:56:12

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ def get_ip(ifname):
 if __name__ == "__main__":
    # ip = socket.gethostbyname(socket.gethostname())
     #ip = get_ip("enp0s3")
+    ip = sys.argv[2]
     if len(sys.argv) < 3:
         print "输入路径,ip"
         exit() 
