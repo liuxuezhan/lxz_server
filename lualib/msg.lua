dcfae88174a7490e13115575a9fa28ca89e9dff4
lxz <253550465@qq.com>
2017-01-18 11:11:24 +0800

    2017-01-18 11:11:24

--- lualib/msg.lua
+++ lualib/msg.lua
@@ -4,48 +4,42 @@ local msg = {}
 msg.cs_login = {"name","pwd","sid","pid"} 
 msg.sc_login = {"nid","pid","host","port"} 
 msg.cs_msg = {"pid","id","msg"} 
-msg.test1 = {"pid","id","msg",{name="table",v="cs_msg",k="pids" },} 
-msg.test2 = {"pid","id","msg",{name="array_table",v="cs_msg",k="pids" },} 
-msg.test2 = {"pid","id","msg",{name="array",k="pids" },} 
+msg.test1 = {"pid","id","msg",{class="table",v="cs_msg",k="pids" },} 
+msg.test2 = {"pid","id","msg",{class="array_table",v="cs_msg",k="pids" },} 
+msg.test3 = {"pid","id","msg",{class="array_sparse",k="pids" },} 
+msg.test4 = {"pid","id","msg",{class="array",k="pids" },} 
 
-local function zip(tab,...) --压缩key
-    local obj = {}
-    local src = {...}
-    for i=1,#tab do
-        local key = tab[i]
-        for j=1,#src do
-            if type(src[j])=="table" then
-                obj[i] = src[j][key]
-            end
-        end
-    end
-    return obj
-end
 
-local function zip2(tab,...) 
+local function zip(tab,...) 
     local obj = {}
     local src = {...}
     for i=1,#tab do
         local key = tab[i]
         if type(key) == "table" then
-            if key.name == "table" then--套单表项
+            if key.class == "table" then--套单表项
                 for j=1,#src do
                     if type(src[j])=="table" then
-                        obj[i] = zip2(msg[key.v],src[j][key.k])
+                        obj[i] = zip(msg[key.v],src[j][key.k])
                     end
                 end
-            elseif key.name == "array_table" then--套多表项
+            elseif key.class == "array" then--套数组
+                for j=1,#src do
+                    if type(src[j])=="table" then
+                        obj[i] = src[j][key]
+                    end
+                end
+            elseif key.class == "array_table" then--套多表项
                 for j=1,#src do
                     if type(src[j])=="table" and type(src[j][key.k])=="table" then
                         obj[i] = {} 
                         for _,v in pairs(src[j][key.k]) do
-                            table.insert(obj[i],zip2(msg[key.v],v))
+                            table.insert(obj[i],zip(msg[key.v],v))
                         end
                     end
                 end
-            elseif key.name == "array" then--套稀疏数组
+            elseif key.class == "array_sparse" then--套稀疏数组
                 for j=1,#src do
-                    if type(src[j])=="table" and type(src[j][key.k])=="table" then
+                    if type(src[j])=="table" then
                         obj[i] = {} 
                         for k,v in pairs(src[j][key.k]) do
                             table.insert(obj[i],{k,v})
@@ -64,16 +58,23 @@ local function zip2(tab,...)
     return obj
 end
 
-
 local function unzip(tab,src)
     local obj = {}
     for i = 1,#tab do
         local key = tab[i]
         if type(key) == "table" then
-            local temp_tab = msg.get(key)
-            local temp_key = key[1]
-            if src[i] then
-                obj[temp_key]  = unzip(temp_tab,src[i])
+            if key.class == "table" then--套单表项
+                obj[key.k] = unzip(msg[key.v],src[i])
+            elseif key.class == "array" then--套数组
+                obj[key.k] = src[i]
+            elseif key.class == "array_table" then--套多表项
+                obj[key.k] = {} 
+                table.insert(obj[key.k],unzip(msg[key.v],src[i]))
+            elseif key.class == "array_sparse" then--套稀疏数组
+                obj[key.k] = {} 
+                for _,v in pairs(src[i]) do
+                    obj[key.k][v[1]] = v[2] 
+                end
             end
         else
             obj[key] = src[i]
@@ -98,31 +99,6 @@ function msg.unpack(src,tab)
     return src
 end
 
-function msg.arr(src) --压缩空洞数组
-      local t = {}
-      for k,v in pairs(src) do
-          table.insert(t,{k,v})
-      end
-      return t
-end
-function msg.unarr(src) 
-      local t = {}
-      for _,v in pairs(src) do
-          t[v[1]]=v[2] 
-      end
-      return t
-end
-
-
-
-function msg.get(tab)
-    local temp = {}
-    for i=2,#tab do
-        temp[i - 1] = tab[i]
-    end 
-    return temp
-end
-
 function msg.zip(what,...) --压缩key
     return zip(msg[what],...)
 end
@@ -130,6 +106,7 @@ end
 function msg.unzip(what,src)
     return unzip(msg[what],src)
 end
+
 function msg.read(fun,fd)
     local ok ,ret = pcall(fun,fd)
     if not ok then
